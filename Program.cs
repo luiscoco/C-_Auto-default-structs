21d64e94d4751bee4e81c7a4ebafb2c025f3df65
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2023-06-12 18:58:16 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Hello, World!");
+
+Point punto = new Point();
+Console.WriteLine(punto.X);
+Console.WriteLine(punto.Y);
+
+public struct Point
+{
+    public int X;
+    public int Y;
+
+    public Point(int x, int y) 
+    {
+        X = x;
+        //Y = y;
+    }
+}
+
+
+
